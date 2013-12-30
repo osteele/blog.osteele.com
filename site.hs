f5083182d6d0ef06272e1dbfd79b71d37921a474
Oliver Steele <steele@osteele.com>
2013-12-30 14:43:35 -0500

    combine index and archive

--- site.hs
+++ site.hs
@@ -33,20 +33,20 @@ main = hakyllWith config $ do
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["archive/index.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+    --create ["archive/index.html"] $ do
+    --    route idRoute
+    --    compile $ do
+    --        posts <- recentFirst =<< loadAll "posts/*"
+    --        let archiveCtx =
+    --                listField "posts" postCtx (return posts) `mappend`
+    --                constField "title" "Archives"            `mappend`
+    --                defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= removeExtensionsFromLocalUrls ".html"
-                >>= relativizeUrls
+    --        makeItem ""
+    --            >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+    --            >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+    --            >>= removeExtensionsFromLocalUrls ".html"
+    --            >>= relativizeUrls
 
     match "index.html" $ do
         route $ setExtension "html"
